f92d1e751dc3471245bfdf658c3327104ec340f3
Marco Peli <xxmarco89xx@gmail.com>
2020-08-13 18:41:30 +0200

    - Added a very basic register handling

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-import {SERVER_PORT} from './src/constants.js'
-
+var constants = require('./src/constants');
+var dbHandler = require('./src/db_handler');
+var registerUtils = require('./src/register_utils');
 var express = require('express');
 var app = express();
-
-const prova = {
-	ciao: "diocan"
-}
 app.use(express.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+	dbHandler.knex.select('*').from('simpleboard-api.login').then(data => {
+		console.log(data)});
 });
 
 app.post('/register', function (req, res) {
-  console.log(req.body.ciao);
-  res.json(prova);
+	let registerData = {
+		login: req.body.email,
+		password: req.body.password
+	}
+	console.log('registerdata:', registerData);
+	dbHandler.registerUser(res, registerData);
 });
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(constants.SERVER_PORT, function () {
+  console.log('Example app listening on port ' + constants.SERVER_PORT);
 });
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,7 @@
-export const SERVER_PORT = 3000;
+exports.SERVER_PORT = 3001;
+exports.LOGIN_SUCCESSFUL = 1;
+exports.LOGIN_FAILED = 0;
+exports.REG_SUCCESSFUL = 1;
+exports.REG_FAILED = 0;
+exports.login = 'pippo';
+exports.password = 'ciao';
